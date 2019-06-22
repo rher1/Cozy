475450c1c70688cd54e3bf3d457c8a7a8c50783a
radwimp <rher1@asu.edu>
2019-06-22 15:23:36 -0700

    poop

--- test.py
+++ test.py
@@ -48,17 +48,13 @@ def cooling():
 # User Input
 desired_temp = int(input("Enter your desired temperature: "))
 
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+cs = digitalio.DigitalInOut(board.D5)
+
+max31855=adafruit_max31855.MAX31855(spi, cs)
+
 while True:
-    spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-    cs = digitalio.DigitalInOut(board.D5)
-    max31855=adafruit_max31855.MAX31855(spi, cs)
-    
     tempC = max31855.temperature
     tempF = tempC * 9 / 5 + 32
+    print('Temperature: {} c {} F '.format(tempC, tempF))
     time.sleep(0.05)
-    if tempF <= desired_temp:
-        print('Temperature: {} c {} F '.format(tempC, tempF))
-        heating()
-    else:
-        print('Temperature: {} c {} F '.format(tempC, tempF))
-        cooling()
