7957ba113ae11c4eae256d47beed3e6464661b5a
radwimp <rher1@asu.edu>
2019-06-24 17:03:04 -0700

    poop

---
+++ gui.py
@@ -0,0 +1,20 @@
+from guizero import App, Text, TextBox, PushButton, Slider, Picture, ButtonGroup
+
+def say_my_name():
+    welcome_message.value = my_name.value
+
+def change_text_size(slider_value):
+    welcome_message.size = slider_value
+
+app = App(title="Hello world")
+
+welcome_message = Text(app, text="Welcome to my app", size=40, font="Times new roman", color="lightblue")
+my_name = TextBox(app, width=30)
+update_text = PushButton(app, command=say_my_name, text="Display my name")
+text_size = Slider(app, command=change_text_size, start=10, end=80)
+my_cat = Picture(app, image="cat.gif")
+
+row_choice = ButtonGroup(app, options=[ ["Front", "F"], ["Middle", "M"],["Back", "B"] ],
+                         selected="M", horizontal=True, grid=[1,2], align="left")
+
+app.display()
