9b14b7fa1043ca8dc253fc8a629e328970c7c31b
radwimp <rher1@asu.edu>
2019-06-24 17:06:20 -0700

    poop

--- gui.py
+++ gui.py
@@ -12,7 +12,6 @@ welcome_message = Text(app, text="Welcome to my app", size=40, font="Times new r
 my_name = TextBox(app, width=30)
 update_text = PushButton(app, command=say_my_name, text="Display my name")
 text_size = Slider(app, command=change_text_size, start=10, end=80)
-my_cat = Picture(app, image="cat.gif")
 
 row_choice = ButtonGroup(app, options=[ ["Front", "F"], ["Middle", "M"],["Back", "B"] ],
                          selected="M", horizontal=True, grid=[1,2], align="left")
