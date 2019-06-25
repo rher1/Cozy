e77bb03b9aeabfa489e403b8cc96ee397ee4366f
radwimp <rher1@asu.edu>
2019-06-24 17:13:17 -0700

    poop

--- test.py
+++ test.py
@@ -57,7 +57,9 @@ try:
         time.sleep(0.05)
         if tempF <= desired_temp:
             heating()
+            print("c, a, end="\r", flush=True)
         else:
             cooling()
+            print("c, b, end="\r", flush=True)
 finally:
     GPIO.cleanup()
